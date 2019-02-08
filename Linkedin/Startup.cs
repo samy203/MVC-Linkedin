c43080e37a59ca7644f578418b4ee9e0d0fb022a
Mostafa Samy <Mostafa.Samy203@gmail.com>
2019-02-08 15:00:04 +0200

    adding sln

---
+++ Linkedin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Linkedin.Startup))]
+namespace Linkedin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

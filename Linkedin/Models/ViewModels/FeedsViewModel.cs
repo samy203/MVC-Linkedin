49b2127f03248a4d60cb53ebf6dea760ee63d6f5
Mostafa Samy <Mostafa.Samy203@gmail.com>
2019-02-17 09:28:10 +0200

    Adding feeds view model

---
+++ Linkedin/Models/ViewModels/FeedsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Linkedin.Models.ViewModels
+{
+    public class FeedsViewModel
+    {
+        public ApplicationUser applicationUser { get; set; }
+    }
+}
